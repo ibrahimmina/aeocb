9c339d19da01b088524dbdf71813dfb2c2bb37c9
visum360 <33985156+visum360@users.noreply.github.com>
2021-04-01 17:57:31 +0200

    major updates

--- core/models.py
+++ core/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.urls import reverse
-
+from django.db.models import signals
+from django.dispatch import receiver
+from PIL import Image, ImageDraw, ImageFont
+import datetime
+from django.core.files.storage import FileSystemStorage
 
 class theme(models.Model):
 
@@ -48,6 +52,12 @@ class verse(models.Model):
     def get_update_url(self):
         return reverse("core_verse_update", args=(self.pk,))
 
+    @property
+    def get_photo_url(self):
+        if self.verse_image and hasattr(self.verse_image, 'url'):
+            return self.verse_image.url
+        else:
+            return "/static/emptyimage.png"
 
 class post(models.Model):
 
@@ -70,6 +80,14 @@ class post(models.Model):
 
     def get_update_url(self):
         return reverse("core_post_update", args=(self.pk,))
+    
+    @property
+    def get_photo_url(self):
+        if self.post_image and hasattr(self.post_image, 'url'):
+            return self.post_image.url
+        else:
+            return "/static/emptyimage.png"
+
 
 
 class qoute(models.Model):
@@ -92,3 +110,54 @@ class qoute(models.Model):
 
     def get_update_url(self):
         return reverse("core_qoute_update", args=(self.pk,))
+
+    @property
+    def get_photo_url(self):
+        if self.qoute_image and hasattr(self.qoute_image, 'url'):
+            return self.qoute_image.url
+        else:
+            return "/static/emptyimage.png"
+
+@receiver(signals.post_save, sender=verse)
+def on_create_or_updated_obj(sender,created, instance, **kwargs):
+    if created:
+
+        verse_theme = theme.objects.filter(theme_month__month=instance.verse_date.month)
+        post_text =  instance.verse_french + "\n" + instance.verse_english + "\n" +  instance.verse_kirundi + "\n" + verse_theme[0].theme_hashtag + "\n" + instance.verse_hashtag + "\n"
+        image = Image.open(instance.verse_image)
+        image_file = 'upload/post_images/verse_' + str(instance.verse_date) + ".png"
+        image.save('media/' + image_file)
+        p = post(post_date=instance.verse_date,post_type="verse",post_text=post_text, post_image=image_file)
+        p.save()
+    else:
+        verse_theme = theme.objects.filter(theme_month__month=instance.verse_date.month)
+        post_text =  instance.verse_french + "\n" + instance.verse_english + "\n" +  instance.verse_kirundi + "\n" + verse_theme[0].theme_hashtag + "\n" + instance.verse_hashtag + "\n"
+
+        post_verse = post.objects.filter(post_date=instance.verse_date,post_type="verse")
+        post_verse.post_text = post_text
+        post_verse.save()
+
+        # create Image object with the input image
+
+        #image = Image.open(instance.verse_image)
+
+        # initialise the drawing context with
+        # the image object as background
+
+        #draw = ImageDraw.Draw(image)
+
+        # create font object with the font file and specify
+        # desired size
+
+        #font = ImageFont.load_default()
+
+        # starting position of the message
+
+        #(x, y) = (150, 150)
+        #name = post_text
+        #color = 'rgb(255, 255, 255)' # white color
+        #draw.text((x, y), name, fill=color, font=font)
+
+        # save the edited image
+
+        #image.save('post_folder/' + str(p.post_date) + ".jpg")
